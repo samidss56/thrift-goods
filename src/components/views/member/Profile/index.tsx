c0a26e6d7a868ce397407c9a2388d648c7655e89
samidss56 <adimas.id@gmail.com>
2024-01-24 19:20:47 +0700

    feat: update user profile and password

--- src/components/views/member/Profile/index.tsx
+++ src/components/views/member/Profile/index.tsx
@@ -9,11 +9,38 @@ import userServices from "@/services/user";
 
 const ProfileMemberView = ({ profile, setProfile, session }: any) => {
   const [changeImage, setChangeImage] = useState<any>({});
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState("");
+
+  const handleChangeProfile = async (e: any) => {
+    e.preventDefault();
+    setIsLoading("profile");
+    const form = e.target as HTMLFormElement;
+    const data = {
+      fullname: form.fullname.value,
+      phone: form.phone.value,
+    };
+    const result = await userServices.updateProfile(
+      profile.id,
+      data,
+      session.data?.accessToken
+    );
+
+    if (result.status === 200) {
+      setIsLoading("");
+      setProfile({
+        ...profile,
+        fullname: data.fullname,
+        phone: data.phone,
+      });
+      form.reset();
+    } else {
+      setIsLoading("");
+    }
+  };
 
   const handleChangeProfilePicture = (e: any) => {
     e.preventDefault();
-    setIsLoading(true);
+    setIsLoading("picture");
     const file = e.target[0]?.files[0];
     if (file) {
       uploadFile(
@@ -31,7 +58,7 @@ const ProfileMemberView = ({ profile, setProfile, session }: any) => {
             );
 
             if (result.status === 200) {
-              setIsLoading(false);
+              setIsLoading("");
               setProfile({
                 ...profile,
                 image: newImageURL,
@@ -39,10 +66,10 @@ const ProfileMemberView = ({ profile, setProfile, session }: any) => {
               setChangeImage({});
               e.target[0].value = "";
             } else {
-              setIsLoading(false);
+              setIsLoading("");
             }
           } else {
-            setIsLoading(false);
+            setIsLoading("");
             setChangeImage({});
           }
         }
@@ -50,89 +77,136 @@ const ProfileMemberView = ({ profile, setProfile, session }: any) => {
     }
   };
 
+  const handleChangePassword = async (e: any) => {
+    e.preventDefault();
+    setIsLoading("password");
+    const form = e.target as HTMLFormElement;
+    const data = {
+      password: form["new-password"].value,
+      oldPassword: form["old-password"].value,
+      encryptedPassword: profile.password,
+    };
+    const result = await userServices.updateProfile(
+      profile.id,
+      data,
+      session.data?.accessToken
+    );
+
+    if (result.status === 200) {
+      setIsLoading("");
+      form.reset();
+    } else {
+      setIsLoading("");
+    }
+  };
+
   return (
     <MemberLayout>
       <h1 className={styles.profile__title}>Profile</h1>
       <div className={styles.profile__main}>
-        <div className={styles.profile__main__avatar}>
-          {profile.image ? (
-            <Image
-              className={styles.profile__main__avatar__image}
-              src={profile.image}
-              alt="User Profile"
-              width={200}
-              height={200}
-            />
-          ) : (
-            <div className={styles.profile__main__avatar__image}>
-              {profile?.fullname?.charAt(0)}
-            </div>
-          )}
-          <form onSubmit={handleChangeProfilePicture}>
-            <label
-              className={styles.profile__main__avatar__label}
-              htmlFor="upload-image"
-            >
-              {changeImage.name ? (
-                <p>{changeImage.name}</p>
-              ) : (
-                <>
-                  <p>Upload a new Avatar</p>
-                  <p>
-                    Maximum upload size is <b>1 MB</b>
-                  </p>
-                </>
-              )}
-            </label>
-            <input
-              className={styles.profile__main__avatar__input}
-              type="file"
-              name="image"
-              id="upload-image"
-              onChange={(e: any) => {
-                e.preventDefault();
-                setChangeImage(e.currentTarget.files[0]);
-              }}
-            />
-            <Button
-              className={styles.profile__main__avatar__button}
-              type="submit"
-              variant="primary"
-            >
-              {isLoading ? "Uploading..." : "Upload"}
-            </Button>
-          </form>
-        </div>
-        <div className={styles.profile__main__detail}>
-          <form action="">
-            <Input
-              label="Fullname"
-              type="text"
-              name="fullname"
-              defaultValue={profile.fullname}
-            />
-            <Input
-              label="Email"
-              type="text"
-              name="email"
-              defaultValue={profile.email}
-            />
-            <Input
-              label="Phone"
-              type="number"
-              name="phone"
-              defaultValue={profile.phone}
-            />
-            {/* <Input
-              label="Password"
-              type="password"
-              name="password"
-              defaultValue={profile.password}
-            /> */}
-            <Button type="submit" variant="primary">
-              Update Profile
-            </Button>
-          </form>
+        <div className={styles.profile__main__row}>
+          <div className={styles.profile__main__row__avatar}>
+            <h2 className={styles.profile__main__row__avatar__title}>Avatar</h2>
+            {profile.image ? (
+              <Image
+                className={styles.profile__main__row__avatar__image}
+                src={profile.image}
+                alt="User Profile"
+                width={200}
+                height={200}
+              />
+            ) : (
+              <div className={styles.profile__main__row__avatar__image}>
+                {profile?.fullname?.charAt(0)}
+              </div>
+            )}
+            <form onSubmit={handleChangeProfilePicture}>
+              <label
+                className={styles.profile__main__row__avatar__label}
+                htmlFor="upload-image"
+              >
+                {changeImage.name ? (
+                  <p>{changeImage.name}</p>
+                ) : (
+                  <>
+                    <p>Upload a new Avatar</p>
+                    <p>
+                      Maximum upload size is <b>1 MB</b>
+                    </p>
+                  </>
+                )}
+              </label>
+              <input
+                className={styles.profile__main__row__avatar__input}
+                type="file"
+                name="image"
+                id="upload-image"
+                onChange={(e: any) => {
+                  e.preventDefault();
+                  setChangeImage(e.currentTarget.files[0]);
+                }}
+              />
+              <Button
+                className={styles.profile__main__row__avatar__button}
+                type="submit"
+                variant="primary"
+              >
+                {isLoading === "picture" ? "Uploading..." : "Upload Picture"}
+              </Button>
+            </form>
+          </div>
+          <div className={styles.profile__main__row__profile}>
+            <h2>Profile</h2>
+            <form onSubmit={handleChangeProfile}>
+              <Input
+                label="Fullname"
+                type="text"
+                name="fullname"
+                defaultValue={profile.fullname}
+              />
+              <Input
+                label="Phone"
+                type="number"
+                name="phone"
+                defaultValue={profile.phone}
+              />
+              <Input
+                label="Email"
+                type="text"
+                name="email"
+                defaultValue={profile.email}
+                disabled
+              />
+              <Input
+                label="Role"
+                type="text"
+                name="role"
+                defaultValue={profile.role}
+                disabled
+              />
+              <Button
+                className={styles.profile__main__row__profile__buttton}
+                type="submit"
+                variant="primary"
+              >
+                {isLoading === "profile" ? "Updating..." : "Update Profile"}
+              </Button>
+            </form>
+          </div>
+          <div className={styles.profile__main__row__password}>
+            <h2>Change Password</h2>
+            <form onSubmit={handleChangePassword}>
+              <Input label="Old Password" type="password" name="old-password" />
+              <Input label="New Password" type="password" name="new-password" />
+              <Button
+                className={styles.profile__main__row__password__button}
+                type="submit"
+                variant="primary"
+              >
+                {isLoading === "password" ? "Updating..." : "Update Password"}
+              </Button>
+            </form>
+          </div>
         </div>
       </div>
     </MemberLayout>
--- src/pages/api/user/profile/[[...user]].ts
+++ src/pages/api/user/profile/[[...user]].ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
 import { retrieveDataById, updateData } from "@/lib/firebase/service";
+import { compare, hash } from "bcrypt";
 
 export default async function handler(
   req: NextApiRequest,
@@ -52,6 +53,22 @@ export default async function handler(
       process.env.NEXTAUTH_SECRET || "",
       async (err: any, decoded: any) => {
         if (decoded) {
+          if (data.password) {
+            const passwordConfirm = await compare(
+              data.oldPassword,
+              data.encryptedPassword
+            );
+            if (!passwordConfirm) {
+              res.status(400).json({
+                status: false,
+                statusCode: 400,
+                message: "failed",
+              });
+            }
+            delete data.oldPassword;
+            delete data.encryptedPassword;
+            data.password = await hash(data.password, 10);
+          }
           await updateData("users", user[0], data, (result: boolean) => {
             if (result) {
               res.status(200).json({
