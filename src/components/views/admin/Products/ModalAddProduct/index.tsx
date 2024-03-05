876af5183cc88acddca9b178310c60d248986c53
samidss56 <adimas.id@gmail.com>
2024-03-05 23:00:26 +0700

    feat: add product

--- src/components/views/admin/Products/ModalAddProduct/index.tsx
+++ src/components/views/admin/Products/ModalAddProduct/index.tsx
@@ -2,10 +2,13 @@ import Button from "@/components/ui/Button";
 import Input from "@/components/ui/Input";
 import Modal from "@/components/ui/Modal";
 import Select from "@/components/ui/Select";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
 import styles from "./ModalAddProduct.module.scss";
 import { Product } from "@/types/product.type";
 import InputFile from "@/components/ui/InputFile";
+import productServices from "@/services/product";
+import { useSession } from "next-auth/react";
+import { uploadFile } from "@/lib/firebase/service";
 
 type Proptypes = {
   setModalAddProduct: Dispatch<SetStateAction<boolean>>;
@@ -18,6 +21,7 @@ const ModalAddProduct = (props: Proptypes) => {
   const [isLoading, setIsLoading] = useState(false);
   const [stockCount, setStockCount] = useState([{ size: "", qty: 0 }]);
   const [uploadedImage, setUploadedImage] = useState<File | null>(null);
+  const session: any = useSession();
 
   const handleStock = (e: any, i: number, type: string) => {
     const newStockCount: any = [...stockCount];
@@ -25,24 +29,100 @@ const ModalAddProduct = (props: Proptypes) => {
     setStockCount(newStockCount);
   };
 
+  const uploadImage = (id: string, form: any) => {
+    const file = form.image.files[0];
+    const newName = "main." + file.name.split(".")[1];
+    if (file) {
+      uploadFile(
+        id,
+        file,
+        newName,
+        "products",
+        async (status: boolean, newImageURL: string) => {
+          if (status) {
+            const data = {
+              image: newImageURL,
+            };
+            const result = await productServices.updateProduct(
+              id,
+              data,
+              session.data?.accessToken
+            );
+            if (result.status === 200) {
+              setIsLoading(false);
+              setUploadedImage(null);
+              form.reset();
+              setModalAddProduct(false);
+              const { data } = await productServices.getAllProducts();
+              setProductsData(data.data);
+              setToaster({
+                variant: "success",
+                message: "Success add product",
+              });
+            } else {
+              setIsLoading(false);
+              setToaster({
+                variant: "danger",
+                message: "Failed add product",
+              });
+            }
+          } else {
+            setIsLoading(false);
+            setToaster({
+              variant: "danger",
+              message: "Failed add product",
+            });
+          }
+        }
+      );
+    }
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+    const form: any = event.target as HTMLFormElement;
+    const data = {
+      name: form.name.value,
+      price: form.price.value,
+      category: form.category.value,
+      status: form.status.value,
+      stock: stockCount,
+      image: "",
+    };
+
+    const result = await productServices.addProduct(
+      data,
+      session.data?.accessToken
+    );
+
+    if (result.status === 200) {
+      uploadImage(result.data.data.id, form);
+    }
+  };
+
   return (
     <Modal onClose={() => setModalAddProduct(false)}>
       <h1>Add Product</h1>
-      <form onSubmit={() => {}} className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <Input
           label="Name"
           name="name"
           type="text"
           placeholder="Insert product name"
         />
-        <Input label="Price" name="price" type="number" />
-        <Input label="Category" name="phone" type="number" />
+        <Input
+          label="Price"
+          name="price"
+          type="number"
+          placeholder="Insert product price"
+        />
         <Select
           label="Category"
           name="category"
           options={[
-            { label: "Men", value: "men" },
-            { label: "Women", value: "women" },
+            { label: "Men", value: "Men's Shoes" },
+            { label: "Women", value: "Women's Shoes" },
           ]}
         />
         <Select
